f69272ca28bda0cd8b2d6b08855a798af93754cb
terminal625 <zpersoncool@gmail.com>
2019-04-20 01:24:37 -0400

    delete empty overlays on buffer change

--- other/example.lisp
+++ other/example.lisp
@@ -26,12 +26,10 @@
 ;;;;
 ;;data is stored in the overlay plist?
 
-(defun remove-empty-overlay (overlay)
-  (when (lem:point=
-         (lem:overlay-start overlay)
-         (lem:overlay-end overlay))
-    (print "removing overlay")
-    (lem:delete-overlay overlay)))
+(defun empty-overlay-p (overlay)
+  (lem:point=
+   (lem:overlay-start overlay)
+   (lem:overlay-end overlay)))
 
 (defparameter *overlays* (make-hash-table :test 'eq))
 (defun remove-overlays-of-buffer (buffer)
@@ -44,6 +42,16 @@
     ;;FIXME::remove the 
     ))
 
+(defun remove-empty-overlays-of-buffer (buffer)
+  (let* ((data (gethash buffer *overlays*))
+         (to-delete (remove-if-not 'empty-overlay-p
+                                   data)))
+    (when to-delete
+      ;;FIXME::plural format string? formatter?
+      (format *output* "~%deleting ~a overlays" (length to-delete))
+      (mapc 'our-delete-overlay
+            to-delete))))
+
 (delete-all-overlays)
 
 (defun our-make-overlay (start end attribute)
@@ -53,11 +61,25 @@
       (lem:add-hook
        (lem:variable-value 'lem:kill-buffer-hook :buffer
                            buffer)
-       'remove-overlays-of-buffer))
+       'remove-overlays-of-buffer)
+      (lem:add-hook
+       (lem:variable-value 'lem:after-change-functions :buffer
+                           buffer)
+       'foo))
     (setf (sucle-attribute-overlay attribute)
 	  overlay)
     (push overlay (gethash buffer *overlays*))
     overlay))
+
+(defparameter *output* *standard-output*)
+(defun foo (&rest rest)
+  (destructuring-bind (start end len) rest
+    (declare (ignore end len))
+    (let ((buffer (lem:point-buffer start)))
+      (remove-empty-overlays-of-buffer buffer))
+    ;;(print rest *output*)
+    ))
+(foo 1 2 3)
 
 (defun our-delete-overlay (overlay)
   (let ((buffer (lem:overlay-buffer overlay)))
@@ -79,7 +101,7 @@
           nil))))
 
 (defun copy-attribute-to-sucle-attribute (attribute)
-  (let ((attribute (lem:ensure-attqribute attribute t)))
+  (let ((attribute (lem:ensure-attribute attribute t)))
     (make-instance
      'sucle-attribute
      :underline-p (lem:attribute-underline-p attribute)
@@ -87,7 +109,7 @@
      :reverse-p (lem:attribute-reverse-p attribute)
      :background (lem:attribute-background attribute)
      :foreground (lem:attribute-foreground attribute)
-     ))))
+     )))
 
 ;;TODO:subclass lem:attribute in order to attach extra data to attributes
 ;;have one attribute per overlay?
